73e0a41f52362615126e602f69cf71d5f2ef478d
Emil Ismaylov <emil-is@yandex.ru>
2025-08-15 12:03:37 +0400

    Tutorials

---
+++ BlazingPizza/Data/PizzaStoreContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlazingPizza.Data
+{
+	public class PizzaStoreContext : DbContext
+	{
+
+		public PizzaStoreContext(DbContextOptions options) : base(options)
+		{
+		}
+
+		public DbSet<Order> Orders { get; set; }
+		public DbSet<Pizza> Pizzas { get; set; }
+		public DbSet<PizzaSpecial> Specials { get; set; }
+		public DbSet<Topping> Toppings { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+
+			modelBuilder.Entity<PizzaTopping>().HasKey(x => new { x.PizzaId, x.ToppingId });
+			modelBuilder.Entity<PizzaTopping>().HasOne<Pizza>().WithMany(x => x.Toppings);
+			modelBuilder.Entity<PizzaTopping>().HasOne(x => x.Topping).WithMany();
+		}
+
+	}
+}
